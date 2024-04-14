973212d5b01e0d9edf8eae50b8ae5295b6291878
Daigo U <go26dev@gmail.com>
2024-04-15 00:52:55 +0900

    add search sandbox

---
+++ SearchSandbox/SearchSandboxApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created on 2024/04/15
+//
+
+import SwiftUI
+
+@main
+struct SearchSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
